9e437509f8c7f44cbf9c0264cb63b704a8968f0c
sungmin-choi <53036141+sungmin-choi@users.noreply.github.com>
2020-11-13 22:59:02 +0900

    bfs 공부한날

--- ex.py
+++ ex.py
@@ -1,24 +1,28 @@
+from collections import deque
 
-def dfs(graph, v, visited):
-    # 현재 노드 방문처리
-    visited[v] = True
-    print(v, end=",")
-    # 현재 노드와 연결된 다른 노그를 재귀적으로 방문
-    for i in graph[v]:
-        if not visited[i]:
-            dfs(graph, i, visited)
+
+def bfs(graph, start, visited):
+    queue = deque([start])
+    visited[start] = True
+    while queue:
+        v = queue.popleft()
+        print(v, end=" ")
+        for i in graph[v]:
+            if not visited[i]:
+                queue.append(i)
+                visited[i] = True
 
 
 graph = [
     [1, 2],
-    [0, 3, 4, 5],
-    [0, 6],
+    [0, 3, 4],
+    [0, 5, 6],
     [1],
     [1],
-    [1],
+    [2],
     [2]
 ]
 
 visited = [False]*9
 
-dfs(graph, 0, visited)
+bfs(graph, 0, visited)
